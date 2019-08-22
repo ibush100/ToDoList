e133276382236a8a5a38fb9f10854c47de3b140c
Ian Bush <ianfracnisbush@gmail.com>
2019-08-22 09:34:14 -0400

    added a style and toggling it off and on

--- Todo List App/assets/js/todos.js
+++ Todo List App/assets/js/todos.js
@@ -1,5 +1,4 @@
 //check off specific todos by clicking 
 $("li").click(function(){
-	$(this).css("color", "grey"); 
-	$(this).css("text-decoration","line-through"); 
-})
+$(this).toggleClass("completed")
+});
